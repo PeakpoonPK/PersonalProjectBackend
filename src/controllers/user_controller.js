9618597423a8b47275a232e699bc360369e4f86c
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-10-16 14:45:39 +0700

    edit profile

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -6,31 +6,41 @@ const { checkUserIdSchema } = require('../validators/user_validator')
 
 exports.updateProfile = async (req, res, next) => {
     try {
-        if (!req.files) {
-            return next(createError('profile image is required'));
+        // if (!req.files) {
+        //     return next(createError('profile image is required'));
+        // }
+        // const response = {};
+        // if (req.files.profileImage) {
+        //     const url = await upload(req.files.profileImage[0].path);
+        //     response.profileImage = url;
+        //     await prisma.user.update({
+        //         data: {
+        //             profileImage: url
+        //         },
+        //         where: {
+        //             id: req.user.id
+        //         }
+        //     })
+        // }
+
+        const oldData = await prisma.user.findFirst({ where: { id: req.user.id } });
+        const newData = req.body
+        const patchData = { ...oldData, ...newData }
+        const updateProfile = await prisma.user.update({
+            data: patchData,
+            where: { id: req.user.id }
         }
-        const response = {};
-        if (req.files.profileImage) {
-            const url = await upload(req.files.profileImage[0].path);
-            response.profileImage = url;
-            await prisma.user.update({
-                data: {
-                    profileImage: url
-                },
-                where: {
-                    id: req.user.id
-                }
-            })
-        }
-        res.status(200).json(response)
+        );
+        res.status(201).json({ updateProfile })
+
+        console.log(patchData)
+
     }
+
+
     catch (err) {
         next(err);
-    } finally {
-        if (req.files.profileImage) {
-            fs.unlink(req.files.profileImage[0].path)
-        }
-    };
+    }
 }
 
 exports.getUserById = async (req, res, next) => {
